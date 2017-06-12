3874c7282720d0806cbadf6379df7e2a4ff16747
Lu <coderambolu@gmail.com>
2017-06-12 18:37:59 +0800

    fix

--- AndroidActivity/app/src/main/java/com/example/rambolu/androidactivity/ListActivity/AnimalAdapter.java
+++ AndroidActivity/app/src/main/java/com/example/rambolu/androidactivity/ListActivity/AnimalAdapter.java
@@ -32,25 +32,13 @@ public class AnimalAdapter extends BaseAdapter {
 
     @Override
     public Object getItem(int position) {
-        return null;
+        return position;
     }
 
     @Override
     public long getItemId(int position) {
         return position;
     }
-
-//    @Override
-//    public View getView(int position, View convertView, ViewGroup parent) {
-//        convertView = LayoutInflater.from(mContext).inflate(R.layout.item_list_animal,parent,false);
-//        ImageView img_icon = (ImageView) convertView.findViewById(R.id.img_icon);
-//        TextView txt_aName = (TextView) convertView.findViewById(R.id.txt_aName);
-//        TextView txt_aSpeak = (TextView) convertView.findViewById(R.id.txt_aSpeak);
-//        img_icon.setBackgroundResource(mData.get(position).getaIcon());
-//        txt_aName.setText(mData.get(position).getaName());
-//        txt_aSpeak.setText(mData.get(position).getaSpeak());
-//        return convertView;
-//    }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
--- AndroidActivity/app/src/main/java/com/example/rambolu/androidactivity/TestActivity.java
+++ AndroidActivity/app/src/main/java/com/example/rambolu/androidactivity/TestActivity.java
@@ -1,14 +1,44 @@
 package com.example.rambolu.androidactivity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ListView;
+
+import com.example.rambolu.androidactivity.ListActivity.Animal;
+import com.example.rambolu.androidactivity.ListActivity.AnimalAdapter;
+
+import java.util.LinkedList;
+import java.util.List;
 
 public class TestActivity extends BaseActivity {
+
+    private List<Animal> animalDate;
+    private Context animalContext;
+    private AnimalAdapter animalAdapter;
+    private ListView animalList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
         Log.d(TAG, "TestActivity--->onCreate");
+        setUI();
+    }
+
+    void setUI() {
+        animalContext = TestActivity.this;
+        animalList = (ListView)findViewById(R.id.testListView);
+        animalDate = new LinkedList<>();
+        animalDate.add(new Animal("主题1","副标题1", R.mipmap.icon1));
+        animalDate.add(new Animal("主题2","副标题2", R.mipmap.icon2));
+        animalDate.add(new Animal("主题3","副标题3", R.mipmap.icon3));
+        animalDate.add(new Animal("主题4","副标题4", R.mipmap.icon4));
+        animalDate.add(new Animal("主题5","副标题5", R.mipmap.icon5));
+        animalDate.add(new Animal("主题6","副标题6", R.mipmap.icon6));
+        animalDate.add(new Animal("主题7","副标题7", R.mipmap.icon7));
+        animalAdapter = new AnimalAdapter(animalDate,animalContext);
+        animalList.setAdapter(animalAdapter);
+
     }
 }
