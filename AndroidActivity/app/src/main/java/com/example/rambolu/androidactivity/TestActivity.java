8d0c7e9bb8fe631051085cd50f6da1d75081bcbe
Lu <coderambolu@gmail.com>
2017-06-14 18:31:30 +0800

    add

--- AndroidActivity/app/src/main/java/com/example/rambolu/androidactivity/TestActivity.java
+++ AndroidActivity/app/src/main/java/com/example/rambolu/androidactivity/TestActivity.java
@@ -3,7 +3,11 @@ package com.example.rambolu.androidactivity;
 import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.rambolu.androidactivity.ListActivity.Animal;
 import com.example.rambolu.androidactivity.ListActivity.AnimalAdapter;
@@ -29,6 +33,12 @@ public class TestActivity extends BaseActivity {
     void setUI() {
         animalContext = TestActivity.this;
         animalList = (ListView)findViewById(R.id.testListView);
+
+        //动态加载顶部View和底部View
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View headView = inflater.inflate(R.layout.view_header, null, false);
+        View footView = inflater.inflate(R.layout.view_footer, null, false);
+
         animalDate = new LinkedList<>();
         animalDate.add(new Animal("主题1","副标题1", R.mipmap.icon1));
         animalDate.add(new Animal("主题2","副标题2", R.mipmap.icon2));
@@ -41,7 +51,22 @@ public class TestActivity extends BaseActivity {
         animalDate.add(new Animal("主题9","副标题9", R.mipmap.icon9));
         animalDate.add(new Animal("主题10","副标题10", R.mipmap.icon10));
         animalAdapter = new AnimalAdapter((LinkedList<Animal>) animalDate,animalContext);
-        animalList.setAdapter(animalAdapter);
 
+        // 添加表头表尾需要卸载set方法
+        animalList.addHeaderView(headView);
+        animalList.addFooterView(footView);
+
+        // 隐藏滚动条
+        animalList.setVerticalScrollBarEnabled(true);
+
+        animalList.setAdapter(animalAdapter);
+        animalList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(animalContext,"你点击了第" + position + "项",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
+
 }
